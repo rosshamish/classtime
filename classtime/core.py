f7e6518e301304bc3bb96479f3baeccd765763fc
Ross Anderson <ross.anderson@ualberta.ca>
2015-02-13 21:03:57 -0500

    Add CORS support

--- classtime/core.py
+++ classtime/core.py
@@ -3,7 +3,9 @@ from classtime import app
 
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restless import APIManager
+from flask.ext.cors import CORS
 
 db = SQLAlchemy(app)
 
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
