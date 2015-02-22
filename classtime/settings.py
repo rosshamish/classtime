146804646e3eaedb2a6ff42b486470f424818440
Ross Anderson <ross.anderson@ualberta.ca>
2015-02-22 14:33:26 -0500

    tests(travis-ci): add .travis.yml for Travis Continuous Integration support

--- classtime/settings.py
+++ classtime/settings.py
@@ -1,5 +1,7 @@
 
 import os
+from classtime.logging import logging
+logging = logging.getLogger(__name__)
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'debug')
 if SECRET_KEY == 'debug':
@@ -8,3 +10,4 @@ else:
     DEBUG = False
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:////tmp/classtime.db')
+logging.info('Using SQLALCHEMY_DATABASE_URI {}'.format(SQLALCHEMY_DATABASE_URI))
